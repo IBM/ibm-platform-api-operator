9df63aac15c0f8cb765f72b858fa02d5d8886e3f
Alice Li <zhuoxi.ibm@gmail.com>
2022-03-14 12:42:33 -0400

    create next-csv.sh & bump version (#193)

---
+++ common/scripts/next-csv.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2022 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This script needs to inputs
+# The CSV version that is currently in dev
+
+CURRENT_DEV_CSV=$1
+NEW_DEV_CSV=$2
+PREVIOUS_DEV_CSV=$3
+
+gsed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/g" RELEASE_VERSION
+gsed -i "s/$PREVIOUS_DEV_CSV/$CURRENT_DEV_CSV/g" PREVIOUS_VERSION
+
+gsed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/g" bundle/manifests/ibm-platform-api-operator.clusterserviceversion.yaml
+gsed -i "s/$PREVIOUS_DEV_CSV/$CURRENT_DEV_CSV/g" bundle/manifests/ibm-platform-api-operator.clusterserviceversion.yaml
+
+gsed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/g" helm-charts/platform-api/Chart.yaml
